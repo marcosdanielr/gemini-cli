f7a68c63a12f02aa3c6659927de7da6f4781ced0
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-09-23 23:08:52 -0300

    feat: type prompt

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,29 @@ mod services;
 
 use services::generate_ai_content_service;
 
+use std::io;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let response =
-        generate_ai_content_service::execute("Explain why Rust is so incredible").await?;
+    println!("Welcome to Gemini CLI");
+    println!("To exit type quit()");
 
-    println!("{}", response);
+    loop {
+        let mut prompt = String::new();
+
+        io::stdin().read_line(&mut prompt).unwrap();
+
+        let prompt = prompt.trim();
+        if prompt.eq_ignore_ascii_case("quit()") {
+            break;
+        }
+
+        println!("...");
+
+        let response = generate_ai_content_service::execute(prompt).await?;
+
+        println!("{}", response);
+    }
 
     Ok(())
 }
