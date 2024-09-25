0b714181fdd6abcd67586eabb1b5fb03c37ab40b
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-09-24 22:59:10 -0300

    feat: save preferences in .config folder

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Welcome to Gemini CLI");
     println!("To exit type 'quit()'");
 
+    let mut config_dir = dirs::home_dir().unwrap();
+    config_dir.push(".config/gemini-cli");
+
+    if !config_dir.exists() {
+        fs::create_dir_all(&config_dir)?;
+    }
+
+    let mut api_key_file_path = config_dir;
+    api_key_file_path.push("api_key.txt");
+
     let mut api_key = String::new();
 
-    if File::open("api_key.txt")
+    if File::open(&api_key_file_path)
         .and_then(|mut file| file.read_to_string(&mut api_key))
         .is_err()
     {
-        println!("Please enter your Gemini API KEY to use this application");
+        println!("Please enter your Gemini API KEY to use this application:");
         io::stdin().read_line(&mut api_key).unwrap();
-
-        println!("...");
-
-        let mut api_key_file = File::create("api_key.txt")?;
-        api_key_file.write_all(api_key.as_bytes())?;
+        let mut api_key_file = File::create(&api_key_file_path)?;
+        api_key_file.write_all(api_key.trim().as_bytes())?;
+        println!("API Key saved.");
     }
 
     println!("Type 'remove_api_key()' to remove the stored API key.");
@@ -41,7 +49,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
 
         if prompt.trim().eq_ignore_ascii_case("remove_api_key()") {
-            fs::remove_file("api_key.txt")?;
+            fs::remove_file(&api_key_file_path)?;
             println!("...");
             break;
         }
